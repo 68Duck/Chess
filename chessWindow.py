2afc6f9a74e899301da6fb18f052b58541393d0c
68Duck <Joshuahenry111@yahoo.co.uk>
2021-03-15 21:22:24 +0000

    Added missing pieces and put pieces at start position.

--- chessWindow.py
+++ chessWindow.py
@@ -8,6 +8,7 @@ class ChessWindow(QMainWindow,uic.loadUiType("chessWindow.ui")[0]):
         super().__init__()
         self.setupUi(self)
         self.initUI()
+        self.setupPieces()
 
     def initUI(self):
         self.squares = []
@@ -19,6 +20,38 @@ class ChessWindow(QMainWindow,uic.loadUiType("chessWindow.ui")[0]):
             for j in range(8):
                 self.createSquare(startX,startY,width,height,i,j)
 
+    def setupPieces(self):
+        for i in range(8):
+            self.setSquarePiece(i*8+6,"pawnW")
+            self.setSquarePiece(i*8+1,"pawnB")
+
+        self.setSquarePiece(7,"rookW")
+        self.setSquarePiece(63,"rookW")
+        self.setSquarePiece(0,"rookB")
+        self.setSquarePiece(56,"rookB")
+
+        self.setSquarePiece(23,"bishopW")
+        self.setSquarePiece(47,"bishopW")
+        self.setSquarePiece(16,"bishopB")
+        self.setSquarePiece(40,"bishopB")
+
+        self.setSquarePiece(15,"knightW")
+        self.setSquarePiece(55,"knightW")
+        self.setSquarePiece(8,"knightB")
+        self.setSquarePiece(48,"knightB")
+
+        self.setSquarePiece(31,"kingW")
+        self.setSquarePiece(24,"kingB")
+
+        self.setSquarePiece(39,"queenW")
+        self.setSquarePiece(32,"queenB")
+
+
+
+    def setSquarePiece(self,squareNumber,pieceName): #square number is 0 indexed. Piece 0 is top left and moved down first
+        pixmap = QPixmap(f"{pieceName}.png")
+        self.squares[squareNumber].setPixmap(pixmap.scaled(64, 64, Qt.KeepAspectRatio))
+
     def createSquare(self,startX,startY,width,height,x,y):
         self.newSquare = QLabel(self,alignment=Qt.AlignCenter)
         self.newSquare.setFixedSize(width,height)
@@ -27,10 +60,7 @@ class ChessWindow(QMainWindow,uic.loadUiType("chessWindow.ui")[0]):
             self.newSquare.setStyleSheet("background-color:#769656; border: 1px solid black;")
         else:
             self.newSquare.setStyleSheet("background-color:#eeeed2; border: 1px solid black;")
-        pixmap = QPixmap("pawnW.png")
-        # pixmap = pixmap.scaledToWidth(64)
-        # pixmap = pixmap.scaledToHeight(64)
-        self.newSquare.setPixmap(pixmap.scaled(64, 64, Qt.KeepAspectRatio))
+        self.squares.append(self.newSquare)
 
 
 
