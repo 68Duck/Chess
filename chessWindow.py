17ba9edeca4ee36032dd64d46964b3a5b291ad9e
68Duck <Joshuahenry111@yahoo.co.uk>
2021-03-16 14:53:59 +0000

    Update to allow pieces' squares to be selected.

--- chessWindow.py
+++ chessWindow.py
@@ -11,14 +11,15 @@ class ChessWindow(QMainWindow,uic.loadUiType("chessWindow.ui")[0]):
         self.setupPieces()
 
     def initUI(self):
+        self.selectedSquare = None
         self.squares = []
-        startX = 50
-        startY = 50
-        width = 75
-        height = 75
+        self.startX = 50
+        self.startY = 50
+        self.width = 75
+        self.height = 75
         for i in range(8):
             for j in range(8):
-                self.createSquare(startX,startY,width,height,i,j)
+                self.createSquare(self.startX,self.startY,self.width,self.height,i,j)
 
     def setupPieces(self):
         for i in range(8):
@@ -51,17 +52,70 @@ class ChessWindow(QMainWindow,uic.loadUiType("chessWindow.ui")[0]):
     def setSquarePiece(self,squareNumber,pieceName): #square number is 0 indexed. Piece 0 is top left and moved down first
         pixmap = QPixmap(f"{pieceName}.png")
         self.squares[squareNumber].setPixmap(pixmap.scaled(64, 64, Qt.KeepAspectRatio))
+        self.squares[squareNumber].pieceName = pieceName
 
     def createSquare(self,startX,startY,width,height,x,y):
         self.newSquare = QLabel(self,alignment=Qt.AlignCenter)
         self.newSquare.setFixedSize(width,height)
         self.newSquare.move(startX+width*x,startY+height*y)
+        self.newSquare.pieceName = None
+        self.newSquare.selected = False
+        # self.newSquare.clicked.connect(self.squareClicked)
         if (x+y)%2==1:
             self.newSquare.setStyleSheet("background-color:#769656; border: 1px solid black;")
+            self.newSquare.colour = "black"
         else:
             self.newSquare.setStyleSheet("background-color:#eeeed2; border: 1px solid black;")
+            self.newSquare.colour = "white"
         self.squares.append(self.newSquare)
 
+    def squareClicked(self,squareNumber):
+        square = self.squares[squareNumber]
+        pieceName = square.pieceName
+        if pieceName is None:
+            return#finish me
+        self.updateSelection(square)
+
+        # if pieceName[len(pieceName)-1:len(pieceName)] == "B": #black piece
+        #     print("black")
+        # else: #white piece
+        #     print("white")
+
+    def showMoves(self,square):
+        pass
+
+    def updateSelection(self,newSelectedSquare):
+        if newSelectedSquare.selected == False:
+            if self.selectedSquare:
+                if self.selectedSquare.colour == "white":
+                    self.selectedSquare.setStyleSheet("background-color:#eeeed2; border: 1px solid black;")
+                else:
+                    self.selectedSquare.setStyleSheet("background-color:#769656; border: 1px solid black;")
+            newSelectedSquare.selected = True
+            newSelectedSquare.setStyleSheet("background-color:#BACA2B;")
+            self.selectedSquare = newSelectedSquare
+            self.showMoves(newSelectedSquare)
+
+
+    def mousePressEvent(self,e):
+        x = e.x()
+        y = e.y()
+        if x > self.startX and x < self.startX+8*self.width:
+            if y > self.startY and y < self.startY+8*self.height:
+                xRange = (x-self.startX)/self.width
+                yRange = (y-self.startY)/self.height
+                for i in range(8):
+                    if i > xRange:
+                        i-=1
+                        xPos = i
+                        break
+                for j in range(8):
+                    if j > yRange:
+                        j-=1
+                        ypos = j
+                        break
+                squareNumber = i*8+j
+                self.squareClicked(squareNumber)
 
 
 
--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from PyQt5.QtWidgets import QApplication
 import sys
 import os
 
+from chessWindow import ChessWindow
 
 sys._excepthook = sys.excepthook
 
 # needed to catch exceptions from the UI
-def my_exception_hook(exctype, value, trace6back):
+def my_exception_hook(exctype, value, traceback):
     print(exctype, value, traceback)
     sys._excepthook(exctype, value, traceback)
     sys.exit(1)
@@ -16,5 +17,5 @@ sys.excepthook = my_exception_hook
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = App()
+    window = ChessWindow()
     sys.exit(app.exec_())
